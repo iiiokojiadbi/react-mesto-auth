ef4e799353e7684ac1bedca2a2ed019749fbd55b
iiiokojiadbi <iiiokojiadbi@yandex.ru>
2020-09-17 00:16:03 +0300

    feat: реализовать корректную проверку токена и отобразить в правильной последовательности защищенный роут

--- src/components/App.js
+++ src/components/App.js
@@ -1,89 +1,92 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
-import { AdventureBoard, PageNotFound, Authentication } from './../pages';
+import { AdventureBoard, PageNotFound, Register, Login } from './../pages';
 import Header from './Header';
 import Footer from './Footer';
 import { ProtectedRoute } from './HOC';
 import useLocalStorage from '../hooks/useLocalStorage';
+import InfoTooltips from './InfoTooltip';
+
+import StatusFetchContext from './../contexts/StatusFetchContext';
 
 import api from '../utils/Api';
 
 function App(props) {
   const { history } = props;
+  const { pathname } = props.location;
   const [token, setToken] = useLocalStorage('jwt');
   const [loggedIn, setLoggedIn] = useState(false);
   const [userInfo, setUserInfo] = useState(null);
   const [fetched, setFetched] = useState(false);
-  const [validToken, setValidToken] = useState(false);
+  const [tokenValid, setTokenValid] = useState(false);
+
+  const checkToken = useCallback((token) => {
+    api
+      .checkUser(`Bearer ${token}`)
+      .then(({ data }) => {
+        setLoggedIn(true);
+        setFetched(true);
+        setTokenValid(true);
+        setUserInfo(data);
+      })
+      .catch((err) => {
+        console.log('Ошибка', err);
+      });
+  }, []);
 
   useEffect(() => {
     if (token && !fetched) {
-      api
-        .checkUser(`Bearer ${token}`)
-        .then(({ data }) => {
-          setLoggedIn(true);
-          setFetched(true);
-          setUserInfo(data);
-        })
-        .catch((err) => {
-          setValidToken(false);
-          console.log('Ошибка', err);
-        });
+      checkToken();
     }
-  }, [token, loggedIn, history, userInfo, fetched]);
+  }, [token, loggedIn, pathname, userInfo, fetched, history, checkToken]);
 
   const handleLogout = () => {
     setToken('');
-    setValidToken(false);
     setFetched(false);
+    setTokenValid(false);
     setLoggedIn(false);
   };
 
   return (
     <div className='page'>
-      <Header userInfo={userInfo} onLogout={handleLogout} />
+      <Header userInfo={userInfo} onLogout={handleLogout} loggedIn={loggedIn} />
       <Switch>
-        <ProtectedRoute
-          exact
-          path='/'
-          component={AdventureBoard}
-          loggedIn={loggedIn}
-        />
-        <Route
-          path='/sign-in'
-          render={(props) =>
-            loggedIn ? (
-              <Redirect to='/' />
-            ) : (
-              <Authentication
+        <StatusFetchContext>
+          <ProtectedRoute
+            exact
+            path='/'
+            component={AdventureBoard}
+            loggedIn={loggedIn}
+            tokenValid={tokenValid}
+          />
+          <Route
+            path='/sign-in'
+            render={(props) => (
+              <Login
+                {...props}
+                onLoggedIn={setLoggedIn}
+                loggedIn={loggedIn}
+                checkToken={checkToken}
+              />
+            )}
+          />
+          <Route
+            path='/sign-up'
+            render={(props) => (
+              <Register
                 {...props}
                 onLoggedIn={setLoggedIn}
                 loggedIn={loggedIn}
               />
-            )
-          }
-        />
-        <Route
-          path='/sign-up'
-          render={(props) => (
-            <Authentication
-              {...props}
-              onLoggedIn={setLoggedIn}
-              loggedIn={loggedIn}
-            />
-          )}
-        />
-        <Route path='*' component={PageNotFound} />
+            )}
+          />
+          {/* <Route path='/' exact component={PageNotFound} /> */}
+        </StatusFetchContext>
       </Switch>
       <Footer />
+      <InfoTooltips />
     </div>
   );
 }
-
-console.log({ _id: '5f6117726ea136001299e470', email: 'klerc21@yandex.ru' });
-console.log({
-  token:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1Z…5NjR9.PrAflQOTJHV6tCyWHaCJKJHS3K7nRVWgxb_wMToAzuA',
-});
 
 export default withRouter(App);

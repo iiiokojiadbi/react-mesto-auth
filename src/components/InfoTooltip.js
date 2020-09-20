65231a67dedad6527dc3fa1077a134e0c9e28b5f
iiiokojiadbi <iiiokojiadbi@yandex.ru>
2020-09-20 12:41:20 +0300

    feat: вынести логику получения настроек в константу

--- src/components/InfoTooltip.js
+++ src/components/InfoTooltip.js
@@ -2,31 +2,7 @@ import React from 'react';
 import { Button } from './ui';
 
 import classnames from 'classnames';
-import errorImg from './../images/error.svg';
-import successImg from './../images/success.svg';
-
-function optionsInfoTooltip(type) {
-  switch (type) {
-    case 'error':
-      return {
-        imgStatus: errorImg,
-        altStatus: 'errorImg',
-        titleStatus: 'Что-то пошло не так! Попробуйте ещё раз.',
-      };
-    case 'login':
-      return {
-        imgStatus: successImg,
-        altStatus: 'successImg',
-        titleStatus: 'Вы успешно авторизовались!',
-      };
-    default:
-      return {
-        imgStatus: successImg,
-        altStatus: 'successImg',
-        titleStatus: 'Вы успешно зарегистрировались!',
-      };
-  }
-}
+import { optionsInfoTooltip } from '../constants';
 
 function InfoTooltip({ isOpen, onClose, type }) {
   const tooltipClasses = classnames({
